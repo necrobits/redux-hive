cda212de7b632c824717d4ae70b00be13d373275
Hai Le <honghaile1988@gmail.com>
2020-10-14 13:58:55 +0200

    Update ApiReduceAddon.js
    added prevAction before calling request and nextAction after getting response.
    ignore success key when state change before and after sending request if no success key found ( POST, PUT method where responses are not important)

--- packages/redux-hive-addon-api/src/ApiReduceAddon.js
+++ packages/redux-hive-addon-api/src/ApiReduceAddon.js
@@ -10,6 +10,9 @@ import {makeActionName} from "./utils";
  *      success: 'name',
  *      error: null,
  *      defaultValue: [],
+ *      nextActionSuccess: () => {}
+ *      nextActionFailure: () => {}
+ *      prevActionRequest: () => {}
  *  }
  * }
  */
@@ -25,32 +28,46 @@ function ApiReduceAddon(config) {
             success,
             error = 'error',
             defaultValue = null,
+            nextActionSuccess = () => {},
+            nextActionFailure = () => {},
+            prevActionRequest = () => {}
         } = reduceCfg;
 
         handlers[loadingActionName] = (state) => {
             state[loading] = true;
-            state[success] = defaultValue;
+            if(success) {
+                state[success] = defaultValue;
+            }
             state[error] = null;
+            prevActionRequest()
             delete state[null];
         };
 
         handlers[successActionName] = (state, action) => {
             state[loading] = false;
-            state[success] = action.payload;
+            if(success) {
+                state[success] = action.payload;
+            }
             state[error] = null;
+            nextActionSuccess()
             delete state[null];
         };
 
         handlers[failureActionName] = (state, action) => {
             state[loading] = false;
-            state[success] = defaultValue;
+            if(success) {
+                state[success] = defaultValue;
+            }
             state[error] = action.payload;
+            nextActionFailure()
             delete state[null];
         };
 
         handlers[resetActionName] = (state) => {
             state[loading] = false;
-            state[success] = defaultValue;
+            if(success) {
+                state[success] = defaultValue;
+            }
             state[error] = null;
             delete state[null];
         };
