4fca50ecfed4dde09956a79243d7f09f5b8fd8ab
Andy Tran <andy.tran11996@gmail.com>
2021-11-22 15:18:09 +0100

    init

--- packages/core/src/createReducer.ts
+++ packages/core/src/createReducer.ts
@@ -4,8 +4,8 @@ import _ from 'lodash';
 import type { Reducer } from 'redux';
 import { Action } from './createAction';
 
-
-export type ReducerMap<S> = { [actionType: string]: Reducer<S> };
+export type StateModifier<S=any, A extends Action<any>=Action<any>> = (state: S | undefined, action: A) => void;
+export type ReducerMap<S> = { [actionType: string]: Reducer<S> | StateModifier<S> };
 type ReducerConfig<S> = {
     initialState: S;
     reducers: ReducerMap<S>;
@@ -17,14 +17,14 @@ export function createReducer<S>({
     reducers,
     usingImmer = true,
 }: ReducerConfig<S>): Reducer<S> {
-  
+
     return (state: S = initialState, action: Action): S => {
         if (!_.has(reducers, action.type)) {
             return state;
         }
         const reducer = reducers[action.type];
         if (!usingImmer) {
-            return reducer(state, action);
+            return reducer(state, action)!;
         }
         const nextState = produce(state, (draft: Draft<S>) => {
             reducer(draft as S, action);
